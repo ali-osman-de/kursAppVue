6344dd82d95934152649806bcac3f0d993b8fc44
ali-osman-de <demirkolluosmanali@gmail.com>
2025-06-22 14:21:08 +0300

    fix: update Firebase configuration to use VITE environment variables

--- kursApp/src/utils/firebase.js
+++ kursApp/src/utils/firebase.js
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: import.meta.env.API_KEY,
-    authDomain: import.meta.env.AUTH_DOMAIN,
-    projectId: import.meta.env.PROJECT_ID,
-    storageBucket: import.meta.env.STORE_BUCKET,
-    messagingSenderId: import.meta.env.MESSAGEING_ID,
-    appId: import.meta.env.APP_ID
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGEING_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
+
 
 initializeApp(firebaseConfig);
 
